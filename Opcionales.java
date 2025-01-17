22a91a9531fd1aacaf6fb30883fb3fc6eca1e050
sergi1307 <sergi13072006@gmail.com>
2025-01-17 10:31:25 +0100

    Opcional 2 terminado con todas sus funcionalidades que nos han pedido

--- Opcionales.java
+++ Opcionales.java
@@ -26,7 +26,32 @@ public class Opcionales {
         System.out.println("El tamaño en bytes es: " + tamanyo + ".");
     }
 
-    public static void opcional2() {};
+    /**
+     * Ejercicio opcional 2: Haz un programa que haga lo siguiente:
+     * Crear un array de nombre edades con 10 elementos de tipo entero.
+     * Llena el vector pidiendo las edades por teclado.
+     * Pedir un número y mostrar la edad que está almacenada en esa posición.
+     * Calcular la suma de los n primeros elementos de edades (parámetro dado por el usuario).
+     */
+    public static void opcional2() {
+        int[] edades = new int[10];
+        int edadBuscar, edadSuma, suma = 0;
+
+        for (int i = 0; i < edades.length; i++) {
+            edades[i] = Leer.leerEntero("Introduzca una edad: ");
+        }
+
+        edadBuscar = Leer.leerEntero("Introduzca la posición de la edad que quiere buscar: ");
+        edadSuma = Leer.leerEntero("Introduzca la cantidad de posiciones que quieres sumar: ");
+
+        System.out.println("La edad en la posición: " + edadBuscar + " és: " + edades[edadBuscar] + ".");
+
+        for (int i = 0; i < edadSuma; i++) {
+            suma = suma + edades[i];
+        }
+
+        System.out.println("La suma de las posiciones es: " + suma + ".");
+    };
 
     public static void opcional3() {};
 
